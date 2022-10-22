67d2101accbcf8ca4191fd20fea649a112a88419
PCS <pcs.engineer.team@gmail.com>
2022-10-22 20:15:45 +0900

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+
+    content: [
+        './index.html',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"M PLUS Rounded 1c"', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/typography')],
+};
